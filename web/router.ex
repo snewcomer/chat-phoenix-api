540ebe7617cb7f753bd80de0331b10c4c2ae3c8d
snewcomer <snewcomer24@gmail.com>
2016-07-25 22:04:29 -0700

    Ready for Heroku3

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,6 @@ defmodule Peepchat.Router do
 
   scope "/api", Peepchat do
     pipe_through :api
-    resources "session" , SessionController, only [:index]
+    resources "session", SessionController, only: [:index]
   end
 end
